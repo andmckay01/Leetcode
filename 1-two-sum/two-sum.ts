6b12b51e71b9d7a1303707eae42922beee517352
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-01 17:36:53 -0700

    Time: 0 ms (100.00%) | Memory: 58.4 MB (8.81%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -2,15 +2,18 @@ function twoSum(nums: number[], target: number): number[] {
 
 const numberToIndex = {}
 
-for (let i = 0; i < nums.length; i++) {
-    numberToIndex[nums[i]] = i; //adding number, and the index of that number, to the object
-}
+// for (let i = 0; i < nums.length; i++) {
+//     numberToIndex[nums[i]] = i; //adding number, and the index of that number, to the object
+// }
 
 for (let i = 0; i < nums.length; i++) {
     let numberNeeded = target - nums[i] //target minus current number
 
-    if (numberToIndex[numberNeeded] != undefined && numberToIndex[numberNeeded] != i) {
+    if (numberToIndex[numberNeeded] != undefined) {
         return [i, numberToIndex[numberNeeded]]
         }
+
+    numberToIndex[nums[i]] = i; //adding number, and the index of that number, to the object
+
     }
 }
