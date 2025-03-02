33576678ab7385875415123dff8fbe9941e7f72d
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-01 17:39:54 -0700

    Time: 2 ms (85.01%) | Memory: 58 MB (11.88%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -2,10 +2,6 @@ function twoSum(nums: number[], target: number): number[] {
 
 const numberToIndex = {}
 
-// for (let i = 0; i < nums.length; i++) {
-//     numberToIndex[nums[i]] = i; //adding number, and the index of that number, to the object
-// }
-
 for (let i = 0; i < nums.length; i++) {
     let numberNeeded = target - nums[i] //target minus current number
 
