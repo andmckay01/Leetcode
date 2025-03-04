5bd6dbc3a3b151460433471954aef2efa49c872f
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-04 12:53:59 -0700

    Time: 43 ms (31.64%) | Memory: 71.8 MB (27.60%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -0,0 +1,33 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+
+    /*
+    we need a hashmap of the number and the index of that number
+    We then use this hashmap to compare the current number to one in our hashmap
+    Check if abs(i-j) <= k and return true
+    */
+
+    let num_to_i = {};
+
+    //finding 2 integers that are the same and sufficiently close (k)
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] in num_to_i) {
+            if (Math.abs(num_to_i[nums[i]] - i) <= k) {
+                return true
+            }
+        }
+
+        num_to_i[nums[i]] = i
+    }
+
+    return false
+
+
+
+    /*
+    Most dictionary operations are O(1) time complexity except:
+    1. Iterating through all keys/values: O(n)
+    2. Object.keys(), Object.values(), Object.entries(): O(n)
+    3. Finding a value without knowing its key: O(n)
+    */
+    
+};
