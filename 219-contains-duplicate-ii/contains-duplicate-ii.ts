a9f15f061fa71853d266c8bfbfce2535a07a3d3f
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-05 11:30:17 -0700

    Time: 21 ms (52.11%) | Memory: 76.6 MB (19.86%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -33,72 +33,25 @@
 // };
 
 
-// //NEW NEW
-// function containsNearbyDuplicate(nums: number[], k: number): boolean {
-//     // Edge cases
-//     if (k <= 0) return false;
-//     if (nums.length <= 1) return false;
-    
-//     const numToIndex = new Map<number, number>();
-    
-//     for (let i = 0; i < nums.length; i++) {
-//         const num = nums[i];
-        
-//         // If we've seen this number before and it's within k positions
-//         if (numToIndex.has(num) && i - numToIndex.get(num)! <= k) {
-//             return true;
-//         }
-        
-//         // Always update the most recent index for this number
-//         numToIndex.set(num, i);
-//     }
-    
-//     return false;
-// }
-
+//NEW NEW
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-    // Edge cases for immediate return
-    if (k <= 0 || nums.length <= 1) return false;
+    // Edge cases
+    if (k <= 0) return false;
+    if (nums.length <= 1) return false;
     
-    // For small k values, using a sliding window with a Set is more efficient
-    if (k < 10000) {
-        const window = new Set<number>();
+    const numToIndex = new Map<number, number>();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
         
-        for (let i = 0; i < nums.length; i++) {
-            // If window size exceeds k, remove the oldest element
-            if (i > k) {
-                window.delete(nums[i - k - 1]);
-            }
-            
-            // If current number is already in our window, we found a duplicate within distance k
-            if (window.has(nums[i])) {
-                return true;
-            }
-            
-            // Add current number to window
-            window.add(nums[i]);
+        // If we've seen this number before and it's within k positions
+        if (numToIndex.has(num) && i - numToIndex.get(num)! <= k) {
+            return true;
         }
         
-        return false;
-    } 
-    
-    // For large k values, the Map approach is more efficient
-    else {
-        const numToIndex = new Map<number, number>();
-        
-        for (let i = 0; i < nums.length; i++) {
-            const num = nums[i];
-            
-            if (numToIndex.has(num)) {
-                const prevIndex = numToIndex.get(num)!;
-                if (i - prevIndex <= k) {
-                    return true;
-                }
-            }
-            
-            numToIndex.set(num, i);
-        }
-        
-        return false;
+        // Always update the most recent index for this number
+        numToIndex.set(num, i);
     }
+    
+    return false;
 }
