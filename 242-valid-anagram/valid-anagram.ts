08e0e31c6b12c486658317b32672ebb5d78f2f72
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 11:36:24 -0700

    Time: 27 ms (25.95%) | Memory: 61.2 MB (8.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,31 +1,34 @@
 function isAnagram(s: string, t: string): boolean {
 
     //an anagram is a word that is rearanged into another word
-    
+
     /*
-    this solution is good with O(n) space complexity but not as good
-    from with memory. We need a hash table (which we will learn later)
+    this solution is good with O(n) space complexity but also O(n) memory complexity which is not as good, we shouldn't need all that memory.
     */
-    //SOLUTION 1, pretty good
-    if(s.length != t.length) {
-        return false
-    }
 
-    let sMap = new Map();
-    let tMap = new Map();
+    return s.split('').sort().join('') == t.split('').sort().join('')
 
-    for(let i=0; i<s.length; i++) {
-        //set chars and increment
-        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
-        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
-    }
 
-    for(const [key, value] of sMap.entries()) {
-        if(value != tMap.get(key)) {
-            return false;
-        }
-    }
+    // //SOLUTION 1, pretty good
+    // if(s.length != t.length) {
+    //     return false
+    // }
 
-    return true
+    // let sMap = new Map();
+    // let tMap = new Map();
+
+    // for(let i=0; i<s.length; i++) {
+    //     //set chars and increment
+    //     sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+    //     tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    // }
+
+    // for(const [key, value] of sMap.entries()) {
+    //     if(value != tMap.get(key)) {
+    //         return false;
+    //     }
+    // }
+
+    // return true
 
 };
