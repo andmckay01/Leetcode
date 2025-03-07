c4a7f1348f5e859e0572075815c4a7494fe19c19
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 11:45:46 -0700

    Time: 11 ms (78.18%) | Memory: 62.4 MB (5.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -32,14 +32,30 @@ function isAnagram(s: string, t: string): boolean {
 
     // return true
 
-    //FINAL SOLUTION:
+    // //FINAL SOLUTION: only works if all letters are lowercase so we can use the ASCII characters
+    // if(s.length !== t.length) return false;
+
+    // const array = new Array(26).fill(0);
+
+    // for(let i = 0; i < s.length; i++) {
+    //     array[s.charCodeAt(i) - 97]++;
+    //     array[t.charCodeAt(i) - 97]--;
+    // }
+
+    // return array.every(freq => !freq);
+
+    //ANOTHER ONE
     if(s.length !== t.length) return false;
 
     const array = new Array(26).fill(0);
 
+    function getAlphabetIndex(char) {
+    return char.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+    }
+
     for(let i = 0; i < s.length; i++) {
-        array[s.charCodeAt(i) - 97]++;
-        array[t.charCodeAt(i) - 97]--;
+        array[getAlphabetIndex(s.charAt(i))]++;
+        array[getAlphabetIndex(t.charAt(i))]--;
     }
 
     return array.every(freq => !freq);
