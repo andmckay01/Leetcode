df4112bb6ad0db37493fd5f306e104f70fac29d1
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 12:05:53 -0700

    Time: 3 ms (82.58%) | Memory: 62.5 MB (16.04%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -12,30 +12,27 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
-    let head = new ListNode(null); // dummy head
-    let current = head; //current is currently at the head
-    let carry = 0 // define carry for later
+    let head = new ListNode(0); //dummy head
+    let node = head; //we'll use this node to perform operations
+    let carry = 0;
 
-    // this ensures we will perform that add operation if any numbers needs adding
     while(l1 || l2 || carry > 0) {
-
-        // we want 0 for these if they do not exist so we can add values that are different digits
-        let l1Value = l1 ? l1.val : 0; 
+        
+        //this will ensure we have 0s to add if the numbers are different lengths
+        let l1Value = l1 ? l1.val : 0;
         let l2Value = l2 ? l2.val : 0;
+        let newValue = l1Value + l2Value + carry; //carry comes from last time
+        carry = 0; //reset carry
 
-        let value = l1Value + l2Value + carry //carry starts at 0
-        carry = 0; //reset carry after we use it
-
-        //perform operations if sum > 9
-        if(value > 9) {
-            carry = 1; //we know the carry will only ever be 1
-            value = value % 10;
+        if(newValue > 9) {
+            newValue = newValue % 10;
+            carry = 1; //add carry
         }
 
-        current.next = new ListNode(value); //this gives us [0, 7] on the first run
-        current = current.next //shift node
+        node.next = new ListNode(newValue); //write new value, this will give us 0 -> num
+        node = node.next; //move node
 
-        //move our pointers
+        //increment the 2 numbers
         if(l1) {
             l1 = l1.next
         }
@@ -44,7 +41,6 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
             l2 = l2.next
         }
     }
-
-    return head.next //shifts our head from [0,7,0,8] to [7,0,8]
+    return head.next //dummy head is 0, head.next will be the start of our new list
 
 };
