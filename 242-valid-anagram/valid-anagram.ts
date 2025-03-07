30791d82bd338604543bb74b9aede13d9a446fc8
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 11:37:43 -0700

    Time: 11 ms (78.18%) | Memory: 57.1 MB (27.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -6,8 +6,30 @@ function isAnagram(s: string, t: string): boolean {
     this solution is good with O(n) space complexity but also O(n) memory complexity which is not as good, we shouldn't need all that memory.
     */
 
-    return s.split('').sort().join('') == t.split('').sort().join('')
+    //SOLUTION 2: not as good
+    // return s.split('').sort().join('') == t.split('').sort().join('')
 
 
+    //SOLUTION 1: pretty good
+    if(s.length != t.length) {
+        return false
+    }
+
+    let sMap = new Map();
+    let tMap = new Map();
+
+    for(let i=0; i<s.length; i++) {
+        //set chars and increment
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    }
+
+    for(const [key, value] of sMap.entries()) {
+        if(value != tMap.get(key)) {
+            return false;
+        }
+    }
+
+    return true
 
 };
