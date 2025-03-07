d6cbf1f6624e9293695387e014ad59e072e2c10d
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 11:47:19 -0700

    Time: 15 ms (58.50%) | Memory: 58.9 MB (17.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -6,56 +6,36 @@ function isAnagram(s: string, t: string): boolean {
     this solution is good with O(n) space complexity but also O(n) memory complexity which is not as good, we shouldn't need all that memory.
     */
 
-    //SOLUTION 2: not as good
-    // return s.split('').sort().join('') == t.split('').sort().join('')
-
-
     //SOLUTION 1: pretty good
-    // if(s.length != t.length) {
-    //     return false
-    // }
+    if(s.length != t.length) {
+        return false
+    }
 
-    // let sMap = new Map();
-    // let tMap = new Map();
+    let sMap = new Map();
+    let tMap = new Map();
 
-    // for(let i=0; i<s.length; i++) {
-    //     //set chars and increment
-    //     sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
-    //     tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
-    // }
+    for(let i=0; i<s.length; i++) {
+        //set chars and increment
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    }
 
-    // for(const [key, value] of sMap.entries()) {
-    //     if(value != tMap.get(key)) {
-    //         return false;
-    //     }
-    // }
+    for(const [key, value] of sMap.entries()) {
+        if(value != tMap.get(key)) {
+            return false;
+        }
+    }
 
-    // return true
+    return true
 
-    // //FINAL SOLUTION: only works if all letters are lowercase so we can use the ASCII characters
-    // if(s.length !== t.length) return false;
-
-    // const array = new Array(26).fill(0);
-
-    // for(let i = 0; i < s.length; i++) {
-    //     array[s.charCodeAt(i) - 97]++;
-    //     array[t.charCodeAt(i) - 97]--;
-    // }
-
-    // return array.every(freq => !freq);
-
-    //ANOTHER ONE
+    //FINAL SOLUTION: only works if all letters are lowercase so we can use the ASCII characters
     if(s.length !== t.length) return false;
 
     const array = new Array(26).fill(0);
 
-    function getAlphabetIndex(char) {
-    return char.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
-    }
-
     for(let i = 0; i < s.length; i++) {
-        array[getAlphabetIndex(s.charAt(i))]++;
-        array[getAlphabetIndex(t.charAt(i))]--;
+        array[s.charCodeAt(i) - 97]++; //charCode for a is 97, b is 98, ...
+        array[t.charCodeAt(i) - 97]--;
     }
 
     return array.every(freq => !freq);
