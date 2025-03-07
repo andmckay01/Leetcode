39d42dad4926b17a38d327a35c8b9069142163b2
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 11:51:17 -0700

    Time: 12 ms (69.22%) | Memory: 67.8 MB (20.66%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,19 +1,22 @@
 function containsDuplicate(nums: number[]): boolean {
 
-    const seen = new Set<number>();
+    // seen 
+    // add
+    // delete
+    // has
 
-    for(let i=0; i < nums.length; i++) {
+    let seen = new Set();
 
-        if(seen.has(nums[i])) {
-            return true
-        }
+    for(let i=0; i<nums.length; i++) {
+
+        if(seen.has(nums[i])) return true
 
         seen.add(nums[i])
     }
 
     return false
-    
+
 };
 
-//.includes is an O(n) operation. so if we call that inside a loop that requires
+//.includes is an O(n) operation. so if we call that inside a loop that requires O(n^2)
 //use a set instead which is an O(1) operation so we are O(n) worst case
