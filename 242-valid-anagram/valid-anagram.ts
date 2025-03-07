0c03dcee46152d53fe1ec742d90df1a952176f6a
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-06 23:16:50 -0700

    Time: 12 ms (73.64%) | Memory: 58.5 MB (17.85%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.ts
@@ -0,0 +1,34 @@
+function isAnagram(s: string, t: string): boolean {
+
+    //an anagram is a word that is rearanged into another word
+    /*
+    this solution is good with O(n) space complexity but not as good
+    from with memory. We need a hash table (which we will learn later)
+    */
+
+    //iterate through
+    //2 hash maps
+    //check that one length is the same as the other before iteration the 2nd
+
+    if(s.length != t.length) {
+        return false
+    }
+
+    let sMap = new Map();
+    let tMap = new Map();
+
+    for(let i=0; i<s.length; i++) {
+        //set chars and increment
+        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    }
+
+    for(const [key, value] of sMap.entries()) {
+        if(value != tMap.get(key)) {
+            return false;
+        }
+    }
+
+    return true
+
+};
