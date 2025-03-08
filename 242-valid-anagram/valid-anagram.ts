8dbe2c34831af30cbb69ee98e36b0784e5de5c63
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-07 21:39:10 -0700

    Time: 4 ms (98.18%) | Memory: 57.1 MB (28.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -7,26 +7,26 @@ function isAnagram(s: string, t: string): boolean {
     */
 
     //SOLUTION 1: pretty good
-    if(s.length != t.length) {
-        return false
-    }
+    // if(s.length != t.length) {
+    //     return false
+    // }
 
-    let sMap = new Map();
-    let tMap = new Map();
+    // let sMap = new Map();
+    // let tMap = new Map();
 
-    for(let i=0; i<s.length; i++) {
-        //set chars and increment
-        sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
-        tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
-    }
+    // for(let i=0; i<s.length; i++) {
+    //     //set chars and increment
+    //     sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+    //     tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+    // }
 
-    for(const [key, value] of sMap.entries()) {
-        if(value != tMap.get(key)) {
-            return false;
-        }
-    }
+    // for(const [key, value] of sMap.entries()) {
+    //     if(value != tMap.get(key)) {
+    //         return false;
+    //     }
+    // }
 
-    return true
+    // return true
 
     //FINAL SOLUTION: only works if all letters are lowercase so we can use the ASCII characters
     if(s.length !== t.length) return false;
