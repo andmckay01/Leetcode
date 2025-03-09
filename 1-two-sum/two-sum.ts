0cec22256007f77918d1eb1eddab12705d5ac340
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-08 23:24:59 -0700

    Time: 0 ms (100.00%) | Memory: 59.1 MB (8.44%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,19 +1,20 @@
 function twoSum(nums: number[], target: number): number[] {
 
-    //will store number and associated index
-    let numToIndex = new Map();
+    let numberToIndex = new Map(); //map is number and then the index of that number
 
     for(let i=0; i<nums.length; i++) {
 
         let numberNeeded = target - nums[i];
-
-        if(numToIndex.has(numberNeeded)) {
-            return [numToIndex.get(numberNeeded), i];
+        
+        //check numbers in index
+        if(numberToIndex.has(numberNeeded)) {
+            return [numberToIndex.get(numberNeeded), i] //get index??
         }
 
-        numToIndex.set(nums[i], i);
+        //add numbers to the map
+        numberToIndex.set(nums[i], i)//number and index of that number
+
     }
 
-    //assume each one has exactly one solution so we do not return 
 
 }
