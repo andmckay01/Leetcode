79ed7461119d1a2a0a9ee51c506cc1b43643a5cd
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-22 17:37:49 -0600

    Time: 0 ms (100.00%) | Memory: 58.3 MB (42.01%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -12,26 +12,31 @@
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
 
-    //lists are already sorted
+    let dummy = new ListNode(0);
+    let current = dummy;
 
-    const dummy = new ListNode(0); //this head must stay here
-    let node = dummy; //this is what we will shift
-
-    while(list1 && list2) {
+    while(list1 && list2) { //while both
         if(list1.val < list2.val) {
-            node.next = list1;
-            list1 = list1.next; //shift list1, why not list1.val?
-        } else { //if equals it doesn't matter
-            node.next = list2;
-            list2 = list2.next //shift list2
+            current.next = list1; //add to list
+            list1 = list1.next; //increment
+        } else {
+            current.next = list2; //add to list
+            list2 = list2.next; //increment
         }
-        node = node.next //shift node
+        current = current.next //increment resulting list
     }
 
-    if(list1) { //when list1 is longer
-        node.next = list1;
-    } else {
-        node.next = list2;
+    while(list1 || list2) {
+        if(list1) {
+            current.next = list1; //add to list
+            list1 = list1.next
+            current = current.next //increment resulting list
+        }
+        if(list2) {
+            current.next = list2; //add to list
+            list2 = list2.next
+            current = current.next //increment resulting list
+        }
     }
 
     return dummy.next
