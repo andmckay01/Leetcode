c95d5ccdc31886eb0616da2b1dbe396d902b9d9c
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-22 13:13:07 -0600

    Time: 2 ms (81.47%) | Memory: 58.9 MB (14.39%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -7,22 +7,15 @@ function twoSum(nums: number[], target: number): number[] {
 
         let numberNeeded = target - nums[i]; //target - value = numberNeeded
 
-        //this needs to be before the setting
+        //this needs to be before the setting otherwise you may use the existing index twice
         if(hashmap.has(numberNeeded)) {
             return [i, hashmap.get(numberNeeded)] //return the current index and the index of number needed
         }
 
         hashmap.set(nums[i], i); //adds value and the index of that value to our hashmap
 
-
-
-        //once this is done, we loop to the next number in the array and try it again. This is O(1) time since we only have to at worst loop through the array once
-
-
-
+        // O(1) constant time since we only have to loop through each value once, worst case
 
     }
-
-
     
 };
