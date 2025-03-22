f8adf5be99524cc5a2a57d629258305c8575672d
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-22 13:28:47 -0600

    Time: 0 ms (100.00%) | Memory: 59.1 MB (6.98%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.ts
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -12,16 +12,15 @@
 
 function reverseList(head: ListNode | null): ListNode | null {
 
-    let prev = null;
+    let prev = null; //initialize previous
 
     while(head) {
-        let nextNode = head.next; //save next pointer we will move to
+        let nextNode = head.next; //save next pointer
         head.next = prev; //reverse pointer
-        prev = head; //prev for next iteration
-        head = nextNode; //increment head
+        prev = head; //shift prev to head
+        head = nextNode; //shift head to nextNode
     }
 
-    return prev
-
-
+    return prev; //when everything is done, return prev which will be the final head that we shifted
+    
 };
