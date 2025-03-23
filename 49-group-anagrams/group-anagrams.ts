287c0dca864e50402b992e047470c395674fd61b
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-22 18:40:42 -0600

    Time: 20 ms (94.62%) | Memory: 69.5 MB (21.66%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.ts
@@ -0,0 +1,45 @@
+function groupAnagrams(strs: string[]): string[][] {
+
+    //accessing a value of an index in an array is constant time
+
+    let groupings = new Map(); //can be empty
+
+    for(let s=0; s<strs.length; s++) { //loop through every string strs[s]
+
+        let group = new Array(26).fill(0); //array with 26 zeros
+
+        for(let c=0; c<strs[s].length; c++) { //loop through every character c
+            group[strs[s].charCodeAt(c) - "a".charCodeAt(0)]++ //"a" is 97
+        }
+
+        //convert to string array (see below for why)
+        const key = group.toString();
+
+        //create the key value if it does not exist
+        if(!groupings.has(key)) {
+            groupings.set(key, []) //key with value of empty array 
+        }
+
+        //add to that key. Get is a map method and push is an array method
+        groupings.get(key).push(strs[s])
+
+    }
+
+    //once this is all done we want to return the values of the groupings
+
+    return [...groupings.values()];
+
+};
+    
+/*
+cannot do the following
+array1 = [1,2,3]
+array2 = array1
+array3 = [1,2,3]
+array2 == array3 //false because these are different arrays in memory
+array1 == array2 //true because therse are the same arrays in memory
+instead, we need to convert these arrays to strings
+
+*/
+
+    
