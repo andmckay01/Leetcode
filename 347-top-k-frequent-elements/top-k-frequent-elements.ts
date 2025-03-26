dd81a3b12fdc380f85dd333013f9cb3873da9230
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-26 12:53:56 -0600

    Time: 17 ms (37.26%) | Memory: 62.4 MB (21.44%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -8,23 +8,20 @@ function topKFrequent(nums: number[], k: number): number[] {
 
     let count = new Map();
     let freq = Array.from({length: nums.length + 1}, () => []) //array with arrays to put elements in, + 1 is necessary
-    console.log(freq);
 
     //add numbers to count
     for(let i=0; i<nums.length; i++) {
         count.set(nums[i], 1 + (count.get(nums[i]) || 0)) //set the number as 1 or 1 + what's already there
     }
-    console.log(count);
 
     //once we have the counts, add elements to the arrays
     for(let [num, frequency] of count) {
         freq[frequency].push(num); //push that number to the frequency array at that count (index)
     }
-    console.log(freq);
 
     //then we iterate through the frequency array in reverse
     let res = [];
-    for(let i=freq.length - 1; i>=0; i--) { //must have -1 otherwise we trie to access more than the array
+    for(let i=freq.length-1; i>=0; i--) { //must have -1 otherwise we trie to access more than the array
         for(let value of freq[i]) {
             res.push(value);
             if(res.length >= k) {
@@ -32,11 +29,16 @@ function topKFrequent(nums: number[], k: number): number[] {
             }
         }
     }
+
     //"It is guaranteed that the answer is unique." -> no edge case for there being too many elements
 
-    //This whole thing is O(n + n + n) = O(n) time complexity 
-    //It also has O(n) space complexity because we are only using memory linearly to solve this
+    /*
+    This whole thing is O(n + n + n) = O(n) time complexity 
+    Time complexity:
+      Map is worst case all numbers (n)
+      freq is worst case all number plus 1 (n+1)
+      res is worst case k
+      So it is O(n + n + 1 + k) = O(n) space complexity
+    */
 
-
-    
 };
