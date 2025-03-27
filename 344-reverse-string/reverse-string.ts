82504c58727bb85dc1c8fa65b5353663d1ac375f
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-27 15:58:54 -0600

    Time: 0 ms (100.00%) | Memory: 62.3 MB (47.24%) - LeetSync

--- 344-reverse-string/reverse-string.ts
+++ 344-reverse-string/reverse-string.ts
@@ -2,20 +2,24 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-
-    let left=0;
-    let right=s.length-1;
-
-    while(left < right) {
-        // let left = s[l]//save left
-        [s[left], s[right]] = [s[right], s[left]] //swap left with right
-        left++, right-- //increment pointers
-    }
-
-    // let i=0;
-    // while(i<s.length) {
-    //     s.unshift(s[i]) //add current character to beginning
-    //     s.splice(i+1,1) // remove one element as we move
-    //     i++ //increment i 
-    // }
+    let l = 0;
+    let r = s.length -1;
+    let temp;
+   while(l < r){
+    temp = s[l];
+    s[l] = s[r];
+    s[r] = temp;
+    l++;
+    r--;
+   }
 };
+
+    // let left=0;
+    // let right=s.length-1;
+
+    // while(left < right) {
+    //     // let left = s[l]//save left
+    //     [s[left], s[right]] = [s[right], s[left]] //swap left with right
+    //     left++, right-- //increment pointers
+    // }
+// };
