ae073488d91421ab8c003a0d0d973172d94cc5e6
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-27 15:49:08 -0600

    Time: 2760 ms (5.52%) | Memory: 66 MB (5.14%) - LeetSync

--- 344-reverse-string/reverse-string.ts
+++ 344-reverse-string/reverse-string.ts
@@ -2,5 +2,16 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    s.reverse();
+
+    //how do modify in place without causing an infinite loop?
+    //instead of moving index value to beginning
+
+    //infinite loop, as we add to the beginning it shifts i so we move infinitely
+    let i=0;
+    while(i<s.length) {
+        s.unshift(s[i]) //add current character to beginning
+        s.splice(i+1,1) // remove one element as we move
+        
+        i++ //increment i 
+    }
 };
