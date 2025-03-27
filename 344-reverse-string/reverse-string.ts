814a0e0cce9d8f2f6bfeaba0a071732869f350de
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-27 16:01:14 -0600

    Time: 3 ms (15.72%) | Memory: 62.6 MB (37.42%) - LeetSync

--- 344-reverse-string/reverse-string.ts
+++ 344-reverse-string/reverse-string.ts
@@ -2,24 +2,25 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    let l = 0;
-    let r = s.length -1;
-    let temp;
-   while(l < r){
-    temp = s[l];
-    s[l] = s[r];
-    s[r] = temp;
-    l++;
-    r--;
-   }
-};
-
-    // let left=0;
-    // let right=s.length-1;
-
-    // while(left < right) {
-    //     // let left = s[l]//save left
-    //     [s[left], s[right]] = [s[right], s[left]] //swap left with right
-    //     left++, right-- //increment pointers
-    // }
+//     let l = 0;
+//     let r = s.length -1;
+//     let temp;
+//    while(l < r){
+//     temp = s[l];
+//     s[l] = s[r];
+//     s[r] = temp;
+//     l++;
+//     r--;
+//    }
 // };
+
+//the below works great but the destructuring array swap takes longer and more memory
+    let left=0;
+    let right=s.length-1;
+
+    while(left < right) {
+        // let left = s[l]//save left
+        [s[left], s[right]] = [s[right], s[left]] //swap left with right
+        left++, right-- //increment pointers
+    }
+};
