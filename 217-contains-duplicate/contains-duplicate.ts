729978539ef540c824505c8e08ef785295afccdb
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-26 19:57:20 -0600

    Time: 10 ms (83.24%) | Memory: 69 MB (30.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,20 +1,13 @@
 function containsDuplicate(nums: number[]): boolean {
 
-    //map: has, set, get (which gets the value of the key
-    //set: has, add, no get
+    let numbers = new Set();
 
-    let numberSet = new Set();
-    for(let i=0; i<nums.length; i++) {
+    for(let i=0; i< nums.length; i++) {
+        if(numbers.has(nums[i])) return true
 
-        if(numberSet.has(nums[i])) { //check set for that number
-            return true
-        }
-
-        numberSet.add(nums[i]); //add to set if we haven't found a duplicate yet
-
+        numbers.add(nums[i])
     }
 
     return false
-
     
 };
