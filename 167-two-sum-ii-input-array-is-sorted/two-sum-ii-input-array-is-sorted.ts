5b3cf4d1e4942a3d40217ef2bc18964b982a0379
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-29 11:58:34 -0600

    Time: 465 ms (11.83%) | Memory: 66.6 MB (5.47%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,26 @@
+function twoSum(numbers: number[], target: number): number[] {
+    
+    let l = 0;
+    let r = numbers.length - 1;
+
+    //since this is already in non-decreasing order
+    //we know that we can simply increment and decrement and check for equality until it eventually finds numbers[l] + numbers[r] = target
+    while(l < r) {
+        let sum = numbers[l] + numbers[r];
+
+        console.log('l: ', l);
+        console.log('r: ', r);
+
+        console.log(sum);
+
+        if(sum == target) {
+            return [l+1, r+1];
+        }
+
+        if(sum > target) { //if greater than, we need a lower number
+            r--;
+        } else { //if less than, we need a higher number
+            l++;
+        }
+    }
+};
