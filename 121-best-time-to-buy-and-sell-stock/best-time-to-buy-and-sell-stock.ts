07054b7c9d2c271ed2c779a2c630ee30d1b73797
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-03-31 08:42:03 -0600

    Time: 2 ms (77.13%) | Memory: 65.8 MB (29.45%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+    let l = 0, r = 1;
+    let maxP = 0;
+
+    while (r < prices.length) {
+        if (prices[l] < prices[r]) {
+            let profit = prices[r] - prices[l];
+            maxP = Math.max(maxP, profit);
+        } else {
+            l = r;
+        }
+        r++;
+    }
+    return maxP;
+};
