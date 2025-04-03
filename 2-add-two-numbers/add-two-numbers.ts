f9488e929b682845b53c2edf9d784d2d1b4b9315
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-03 17:49:51 -0600

    Time: 5 ms (44.10%) | Memory: 62.5 MB (42.06%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -12,28 +12,34 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
-    //use the dummy head pattern so that we don't lose our spot
-    let dummyHead = new ListNode(0);
-    let node = dummyHead;
+    /*
+    we need a carry
+    we need to get the remainder 
+    we need to set the values so that we have zeros
+    we need to add them together starting at the current list node and then increment htem
+    we need to add this to a new
+    */
+
     let carry = 0;
+    let dummy = new ListNode(null);
+    let node = dummy;
 
     while(l1 || l2 || carry > 0) {
 
-        //need these so that we have zeros if they do not exist
         let l1Value = l1 ? l1.val : 0;
         let l2Value = l2 ? l2.val : 0;
-
-        let newValue = l1Value + l2Value + carry;
-        carry = 0; //reset carry
+        let newValue = l1Value + l2Value + carry
+        carry = 0; //remember to reset carry once we use it in newValue
 
         if(newValue > 9) {
+            newValue = newValue % 10;
             carry = 1;
-            newValue = newValue % 10; //will remove 10 from newValue
         }
 
-        node.next = new ListNode(newValue); //add next node with newValue
-        node = node.next //move head
+        node.next = new ListNode(newValue);
+        node = node.next; //increment
 
+        //increment our initial linked lists
         if(l1) {
             l1 = l1.next
         }
@@ -42,8 +48,9 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
             l2 = l2.next
         }
 
+
     }
 
-    return dummyHead.next; //dummyHead is 0, next is the start of our list
+    return dummy.next;
     
 };
