0193b108b1eb04ca36325005c224a66fe2b8a73e
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-05 22:19:36 -0600

    Time: 2 ms (76.69%) | Memory: 65.6 MB (44.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -1,15 +1,29 @@
-function maxProfit(prices: number[]): number {
-    let l = 0, r = 1;
-    let maxP = 0;
+// function maxProfit(prices: number[]): number {
+//     let l = 0, r = 1;
+//     let maxP = 0;
 
-    while (r < prices.length) {
-        if (prices[l] < prices[r]) {
-            let profit = prices[r] - prices[l];
-            maxP = Math.max(maxP, profit);
-        } else {
-            l = r;
-        }
-        r++;
+//     while (r < prices.length) {
+//         if (prices[l] < prices[r]) {
+//             let profit = prices[r] - prices[l];
+//             maxP = Math.max(maxP, profit);
+//         } else {
+//             l = r;
+//         }
+//         r++;
+//     }
+//     return maxP;
+// };
+
+//above is the 2 pointer solution. It's pretty good. But below is dynamic programming which is faster and less verbose
+
+function maxProfit(prices: number[]): number {
+    let profit = 0
+    let min = prices[0]
+
+    for (let i = 1; i < prices.length; i++) {
+        profit = Math.max(profit, prices[i] - min)
+        min = Math.min(min, prices[i])
     }
-    return maxP;
+
+    return profit
 };
