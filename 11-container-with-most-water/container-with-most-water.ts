b3c734607ee014637f1337eaac054ff27cab5a87
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-07 13:23:08 -0600

    Time: 3 ms (54.64%) | Memory: 65.4 MB (23.26%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,27 +1,22 @@
 function maxArea(height: number[]): number {
 
-    /*
-    2 pointer problem where we start on either end
-    we only shift the lower of the 2 pointers
-    this will be optimal and eventually arrive at the best solution
-    */
-
     let l = 0;
-    let r = height.length-1;
-    let max = 0;
+    let r = height.length - 1;
+    let maxVolume = 0;
 
     while(l < r) {
-        let area = (r-l) * Math.min(height[l], height[r]); //creates a rectangle
 
-        if(area > max) max = area
+        let minHeight = Math.min(height[l], height[r]);
+        maxVolume = Math.max(maxVolume, minHeight * (r - l));
 
-        if(height[l] >= height[r]) {
-            r--
-        } else {
+        if(height[l] < height[r]) {
             l++
+        } else {
+            r--
         }
+
     }
 
-    return max
+    return maxVolume;
     
 };
