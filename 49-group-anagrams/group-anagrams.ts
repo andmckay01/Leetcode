945deb02c2b093c5170ebb991f3db3db77f95ce6
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-07 13:56:43 -0600

    Time: 259 ms (5.02%) | Memory: 70.8 MB (12.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,34 +1,44 @@
 function groupAnagrams(strs: string[]): string[][] {
 
     /*
-    need a way to determine if each string has the same amount of characters
-
-    loop through the strings, determine characters, collapse this into a key, save the string to the array of arrays with the key
-
-    return using spread operator for every group within the array
+    lowercase english letters -> dictionary where 
+    1. count characters
+    2. construct key
+    3. add to a map
+    4. return the words in this map
     */
 
-    const result = new Map();
+    let grouped = new Map<string, string[]>();
 
     for(let i=0; i<strs.length; i++) {
 
-        const word = strs[i];
+        const word: string = strs[i]
 
-        const letterCount = new Array(26).fill(0); //array with 26 entries and all 0
-        for(let c=0; c<word.length; c++) {
-            const charCode = word[c].charCodeAt(0) - "a".charCodeAt(0);
-            letterCount[charCode]++; //add one to that index
+        const characterArray = new Array(26).fill(0); //array with 26 0's
+
+        for(let c of word) {
+
+            characterArray[c.charCodeAt(0) - "a".charCodeAt(0)]++;
+
         }
 
-        const key = letterCount.toString(); //collapse array into the letter count
-        
-        //if result has key, spread the word into existing. if not, set a new one
-        result.set(key, result.has(key) ? [...result.get(key), word] : [word]);
+        console.log(characterArray);
+
+        let key = characterArray.toString();
+
+        //if it exists, we add to that
+        //if not, we create a new one
+
+        if(grouped.has(key)) {
+            grouped.set(key, [...grouped.get(key), word])
+            console.log(grouped)
+        } else {
+            grouped.set(key, [word])
+        }
 
     }
 
-    return [...result.values()]
-
+    return [...grouped.values()]
 
     
 };
