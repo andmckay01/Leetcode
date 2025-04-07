c8760ce0f33e1918cf75f76dc48f45bd2a15cc57
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-07 14:33:37 -0600

    Time: 1 ms (97.90%) | Memory: 57.9 MB (76.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -16,8 +16,6 @@ function isPalindrome(s: string): boolean {
     let l = 0;
     let r = s.length - 1;
 
-    // let lowercase = s.toLowerCase();
-
     while(l < r) {
 
         //increment until we have a alphaNum characters
