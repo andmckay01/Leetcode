c4af30daca70e72f463657d75baa610b5128d7b5
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-11 07:59:53 -0600

    Time: 28 ms (11.86%) | Memory: 68.8 MB (5.09%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,44 +1,41 @@
 function topKFrequent(nums: number[], k: number): number[] {
 
     /*
-    need a map with the value and the count of those values
-    need an array of arrays where each index is the count and each value is an array of the elements we found (need to return those elements)
-
+    steps:
+    1. iterate through the numbers and add the count to a map
+    2. add the numbers to the array with the index as the count
+    3. iterate in reverse, adding the numbers to a res, and then return
     */
 
-    let count = new Map();
-    let freq = Array.from({length: nums.length + 1}, () => []) //array with arrays to put elements in, + 1 is necessary
+    let count = new Map(); //number: count
+    let freq = Array.from({length: nums.length + 1}, () => [])
 
-    //add numbers to count
+    console.log(freq);
+
+    //count the occurence of each number
     for(let i=0; i<nums.length; i++) {
-        count.set(nums[i], 1 + (count.get(nums[i]) || 0)) //set the number as 1 or 1 + what's already there
-    }
-
-    //once we have the counts, add elements to the arrays
-    for(let [num, frequency] of count) {
-        freq[frequency].push(num); //push that number to the frequency array at that count (index)
-    }
-
-    //then we iterate through the frequency array in reverse
-    let res = [];
-    for(let i=freq.length-1; i>=0; i--) { //must have -1 otherwise we trie to access more than the array
-        for(let value of freq[i]) {
-            res.push(value);
-            if(res.length >= k) {
-                return res;
-            }
+        if(count.has(nums[i])) {
+            count.set(nums[i], count.get(nums[i]) + 1);
+        } else {
+            count.set(nums[i], 1);
         }
     }
 
-    //"It is guaranteed that the answer is unique." -> no edge case for there being too many elements
+    //add to the freq array with the index being our count
+    for(let [num, frequency] of count) {
+        freq[frequency] = [...freq[frequency], num]
+        // freq[frequency] = num;
+    }
 
-    /*
-    This whole thing is O(n + n + n) = O(n) time complexity 
-    Time complexity:
-      Map is worst case all numbers (n)
-      freq is worst case all number plus 1 (n+1)
-      res is worst case k
-      So it is O(n + n + 1 + k) = O(n) space complexity
-    */
-
+    //iterate in reverse
+    let res = [];
+    for(let i=freq.length-1; i>=0; i--) {
+        for(let value of freq[i]) {
+            res.push(value)
+            if(res.length >= k) {
+                return res
+            }
+        }
+    }
+    
 };
