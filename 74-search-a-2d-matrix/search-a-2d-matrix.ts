76675629192066415a2e887cc96e7a6840c32b60
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-12 12:56:57 -0600

    Time: 32 ms (5.54%) | Memory: 62.3 MB (5.28%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -0,0 +1,66 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+
+    /*
+    non-descending order tells us we can use binary search
+    first find the row it's in
+    then find the value in the row
+    if nothing finds, then we return false
+    */
+
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
+
+    let topRow = 0;
+    let bottomRow = ROWS - 1;
+
+    console.log(matrix);
+    console.log('problem target: ', target)
+
+    while(topRow <= bottomRow) {
+        let currentRow = Math.floor((topRow + bottomRow) / 2);
+
+        console.log('current row: ', currentRow);
+
+        //if the target is within the row
+
+        console.log('matrix current row 0: ', matrix[currentRow][0]);
+        console.log('matrix current row cols - 1: ', matrix[currentRow][COLS - 1])
+
+        if(target >= matrix[currentRow][0] && target <= matrix[currentRow][COLS - 1]) {
+            //use binary search again
+
+            console.log('inside');
+            let l = 0;
+            let r = matrix[currentRow].length - 1;
+
+            while(l <= r) { //this will break if l and r move past one another
+
+                const m = Math.floor((l + r) / 2);
+                console.log('INSIDE START:')
+                console.log('l: ', l);
+                console.log('r: ', r);
+                console.log('current value: ', m);
+                console.log('target: ', target);
+                console.log('inside check: ', matrix[currentRow][m])
+                if (target > matrix[currentRow][m]) {
+                    l = m + 1
+                } else if (target < matrix[currentRow][m]) {
+                    r = m - 1
+                } else {
+                    return true //if the m == target
+                }
+            }
+        return false 
+        
+        } else if (target > matrix[currentRow][COLS - 1]) {
+            topRow = currentRow + 1;
+        } else if (target < matrix[currentRow][0]) {
+            bottomRow = currentRow - 1;
+        } else {
+            break;
+        }
+    }
+
+    return false //return false if the value is outside of what we see on the endpoints of rows
+    
+};
