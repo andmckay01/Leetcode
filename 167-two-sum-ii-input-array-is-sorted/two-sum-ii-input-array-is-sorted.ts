448bf706a09cf50a323a0e60e0d1624ba242adc3
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-16 14:10:04 -0600

    Time: 3 ms (42.42%) | Memory: 57.3 MB (45.72%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,21 +1,29 @@
 function twoSum(numbers: number[], target: number): number[] {
-    
+
+    /*
+    sorted: tells us to use 2 pointers..use l and r and increment as we add them together to make target
+    remember to add 1 to result array indices
+    exactly one solution
+    */
+
     let l = 0;
     let r = numbers.length - 1;
 
-    //since this is already in non-decreasing order
-    //we know that we can simply increment and decrement and check for equality until it eventually finds numbers[l] + numbers[r] = target
+    //less than because we can't use the same number twice
     while(l < r) {
+        
         let sum = numbers[l] + numbers[r];
 
-        if(sum == target) {
-            return [l+1, r+1];
+        if(sum > target) {
+            r--;
+        } else if(sum < target) {
+            l++;
+        } else {
+            return [l+1, r+1]; //sorted so we know this is the correct order
         }
 
-        if(sum > target) { //if greater than, we need a lower number
-            r--;
-        } else { //if less than, we need a higher number
-            l++;
-        }
     }
+
+    //no return needed here because we are guaranteed exactly one solution
+    
 };
