81f804d7f5b1e9bef8b5cc041a5c116ca4bd6083
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-16 13:56:49 -0600

    Time: 76 ms (5.03%) | Memory: 66.3 MB (5.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.ts
+++ 907-koko-eating-bananas/koko-eating-bananas.ts
@@ -1,35 +1,39 @@
 function minEatingSpeed(piles: number[], h: number): number {
 
     /*
-    Every time you choose to eat you spend an hour no matter what
-     -> this shows up as rounding to the nearest int Math.ceil (round up)
-
-     Binary search
-     Loop until we find a value where we spend the exact hours
-     Otherwise, return the integer k (minimum integer to be able to eat all bananas in hours (h))
+    If koko eats one pile she must eat that pile and only that pile for that hour, round with ceil
+    our L and R pointers will be the amount of bananas to eat (k), set at min and max piles
+    after we count hours used, we check if this is valid (hours <= h) and if so, update res
+    -> then we make k more agressive (by incrementing l higher)
+    else we make k less agressive by incrementing r lower
     */
 
     let l = 0;
-    let r = Math.max(...piles); //remember to spread the array;
-    let res = r;
+    let r = Math.max(...piles);
+    let kResponse = r; //the amount of bananas koko eats each hour, defaulted to worst case
 
     while(l <= r) {
-        const k = Math.ceil((l + r) / 2);
 
-        let hours = 0;
+        let hoursUsed = 0;
+        let k = l + Math.ceil((r - l) / 2); //midpoint we want to try
+
         for(let p of piles) {
-            hours += Math.ceil(p / k);
-        }
+            hoursUsed += Math.ceil(p / k); //how many hours we use to eat each pile
+        };
 
-        if(hours <= h) {
-            res = Math.min(res, k);
-            r = k - 1;
+        console.log('hoursUsed: ', hoursUsed);
+            console.log('kResponse: ', kResponse);
+
+        if(hoursUsed <= h) {
+            kResponse = Math.min(kResponse, k); //update response
+            console.log('kResponse update: ', kResponse);
+
+            r = k - 1; //increment to eat more (more aggressive)
         } else {
-            l = k + 1;
+            l = k + 1; //increment to eat more
         }
-
     }
 
-    return res;
+    return kResponse;
     
 };
