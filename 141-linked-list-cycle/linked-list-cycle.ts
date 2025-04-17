e51587f07b3a4bd3b20af9fe08fb1b481ddba11d
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-17 13:05:13 -0600

    Time: 58 ms (27.37%) | Memory: 56.6 MB (92.58%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.ts
+++ 141-linked-list-cycle/linked-list-cycle.ts
@@ -13,13 +13,17 @@
 function hasCycle(head: ListNode | null): boolean {
 
     /*
-    we can use a hashmap where we have ListNodes as the values and check that for
-    already existin list nodes. 
+    toirtise and hare algorithm
+    if at any point the hare is before the toirtise we know there is a cycle
 
-    or we can use the tortoise and the hare algorithm to figure it out
+    initialize
+    loop
+      inside the loop we are checking for the fast.next == slow
 
-    iterate through
-    check if the slow ListNode ever equals the fast ListNode
+    slow start at 0 and increment by 1
+    fast start at 1 and increment by 2
+
+    stop once slow and fast equals each other
     */
 
     let slow = head;
@@ -27,13 +31,19 @@ function hasCycle(head: ListNode | null): boolean {
 
     while(fast && fast.next) {
 
-        if(fast.next == slow) return true
+        // console.log('fast: ', fast);
+        // console.log('fast.next: ', fast.next);
 
+        if(fast.next == slow) {
+            return true;
+        }
+
+        //increment
         slow = slow.next;
         fast = fast.next.next;
 
     }
 
-    return false
+    return false;
     
 };
