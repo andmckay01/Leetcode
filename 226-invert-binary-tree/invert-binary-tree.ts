cc0fad4533b385e2f7b9067ecb507d411fbd53f8
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-17 12:45:30 -0600

    Time: 0 ms (100.00%) | Memory: 55.9 MB (65.84%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.ts
+++ 226-invert-binary-tree/invert-binary-tree.ts
@@ -15,23 +15,20 @@
 function invertTree(root: TreeNode | null): TreeNode | null {
 
     /*
-    use recursion on a function that:
-    1. looks at a node
-    2. swaps children
-    3. recursively call our function
+    use recursion where we call the function that inverts for every single node
     */
 
     if (!root) return null;
 
-    //swap trees
+    //invert
     let tmp = root.left;
     root.left = root.right;
     root.right = tmp;
 
-    //resursively call the invertTree function
+    //recurse
     invertTree(root.left);
     invertTree(root.right);
 
-    return root
+    return root;
     
 };
