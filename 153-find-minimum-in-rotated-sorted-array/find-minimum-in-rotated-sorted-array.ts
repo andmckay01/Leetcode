6b69e321d9020ca91f6eedc83965ba381e7d374a
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-17 14:05:36 -0600

    Time: 0 ms (100.00%) | Memory: 54.3 MB (88.27%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,19 +1,10 @@
 function findMin(nums: number[]): number {
 
-    /*
-    binary search
-    left and right
-    We want L < R to simply work, which means we want L = R to break out loop
-    */
-
     let l = 0;
     let r = nums.length - 1;
-    let m = nums[l];
 
     while(l < r) {
-
-        m = l + Math.floor((r - l) / 2);
-        // m = Math.floor((r + l) / 2);
+        let m = l + Math.floor((r-l) / 2); //midpoint without bit overflow
 
         if(nums[r] > nums[m]) {
             r = m;
@@ -22,7 +13,6 @@ function findMin(nums: number[]): number {
         }
     }
 
-    return nums[l];
+    return nums[l]
 
-}
-
+};
