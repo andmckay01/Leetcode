c03a3a93e8569084e40ca98f2092647a89d4f296
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-17 14:49:56 -0600

    Time: 7 ms (88.96%) | Memory: 57.7 MB (75.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,44 +1,22 @@
 function isAnagram(s: string, t: string): boolean {
 
+    /*
+    consist of lowercase english letters (no characters)
+    we can simply iterate through both of these O(n + m) time space -> reduces to O(n)
+    if the inputs contain unicode characters we would use
+    */
+
     if(s.length !== t.length) return false
 
-    let characterArray = new Array(26).fill(0); //array with all zeros
+    let characterArray = new Array(26).fill(0); //array with 26 characters
 
     for(let i=0; i<s.length; i++) {
-        //add character at position for s, remove for t
-        characterArray[s.charCodeAt(i) - 97]++;
-        characterArray[t.charCodeAt(i) - 97]--;
+
+        characterArray[s[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+        characterArray[t[i].charCodeAt(0) - "a".charCodeAt(0)]--
 
     }
 
     return characterArray.every(freq => !freq);
     
 };
-
-    //an anagram is a word that is rearanged into another word
-
-    /*
-    this solution is good with O(n) space complexity but also O(n) memory complexity which is not as good, we shouldn't need all that memory.
-    */
-
-    //SOLUTION 1: pretty good
-    // if(s.length != t.length) {
-    //     return false
-    // }
-
-    // let sMap = new Map();
-    // let tMap = new Map();
-
-    // for(let i=0; i<s.length; i++) {
-    //     //set chars and increment
-    //     sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
-    //     tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
-    // }
-
-    // for(const [key, value] of sMap.entries()) {
-    //     if(value != tMap.get(key)) {
-    //         return false;
-    //     }
-    // }
-
-    // return true
