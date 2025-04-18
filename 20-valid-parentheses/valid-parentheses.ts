ea4fa0866d28f0e2b8b9dbcfd89c3a0d2eb075bd
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-18 12:59:57 -0600

    Time: 4 ms (38.03%) | Memory: 56.3 MB (92.50%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -1,27 +1,32 @@
 function isValid(s: string): boolean {
 
     /*
-    order matters -> stack
-    type matters -> dictionary (map)
+    need a mapping object we use to determine what parenthesis is required
+    use a stack to add things to the stack then remove them in the proper order
+
+    1. loop through, add to stack if open
+    2. if close, check if corresponding parenthesis exists and pop from stack
+    3. if at any point the condition does not work, return false
+    4. if at the end our stack is empty, return true
     */
 
-    let closeToOpen = {")": "(", "}": "{", "]": "["};
-    let stack = [];
+    let closeToOpen = {")": "(", "]": "[", "}": "{"};
+    let stack: string[] = [];
 
-    for(let bracket of s) {
-    
-        if(!closeToOpen[bracket]) {
-            stack.push(bracket);
+    for(let i=0; i<s.length; i++) {
+        if(!closeToOpen[s[i]]) { //if opening parenthesis
+            stack.push(s[i]);
         } else {
-            if(closeToOpen[bracket] !== stack[stack.length - 1] || stack.length == 0) {
-                return false
-            } else {
+            //maps current closing to the open and checks if it exists at the end of stack
+            if(closeToOpen[s[i]] == stack[stack.length - 1]) {
                 stack.pop();
+            } else {
+                return false
             }
         }
     }
 
-    if(stack.length == 0) {
+    if(stack.length == 0) { //if we loop through everything and the stack is empty
         return true
     } else {
         return false
