22a9ef3d4796120e3023538ed76c21ad7c4d6d0f
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-20 15:55:07 -0600

    Time: 2 ms (53.27%) | Memory: 61.9 MB (25.10%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.ts
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+
+    /*
+    balanced binary tree is one where every node's children differ in height by no more than 1
+    so we can use a recursive dfs to find the heights
+    compare those heights
+    if at any point they are not balanced, we will return false
+    */
+
+    function dfs(node) {
+
+        if(!node) return [true, 0]
+
+        //recursive call
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+
+        //find height and return
+        const height =  1 + Math.max(left[1], right[1]);
+
+        // If either subtree is unbalanced, the whole tree is unbalanced
+        if (!left[0] || !right[0]) {
+            return [false, 0]; // Height doesn't matter if already unbalanced
+        }
+
+        if(Math.abs(right[1] - left[1]) > 1) {
+            return [false, height]
+        } else {
+            return [true, height]
+        }
+
+    }
+
+    let result = dfs(root);
+    return result[0]
+    
+};
