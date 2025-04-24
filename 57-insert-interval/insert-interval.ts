0bd792065b95f0b479ecf3a29ec870ecb432e081
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-24 10:04:50 -0600

    Time: 6 ms (30.94%) | Memory: 59.7 MB (46.64%) - LeetSync

--- 57-insert-interval/insert-interval.ts
+++ 57-insert-interval/insert-interval.ts
@@ -14,24 +14,17 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
     //check if it doesn't overlap
     for(let i = 0; i < intervals.length; i++) {
 
-        console.log(res);
-
-        console.log('new interval: ', newInterval);
-        console.log('current interval: ', intervals[i]);
-
         //check if end of new interval is less than start of current interval
         if (newInterval[1] < intervals[i][0]) {
             res.push(newInterval);
             
             //no overlapping intervals means that if the newInterval does not overlap, we are done
             res.push(...intervals.slice(i));
-            console.log(res);
             return res;
         
         //check if end of new interval is greater than the start of current interval
         } else if (newInterval[0] > intervals[i][1]) {
             //append this because it didn't overlap
-            console.log('push interval');
             res.push(intervals[i]);
 
         //this is now our case if it does overlap
@@ -39,9 +32,7 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
 
             //if it overlaps we take the min of both and the max of both and combine them
             newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])]
-
-            console.log('new interval: ', newInterval);
-
+            
             //we don't save to res yet because this new interval might overlap with our next interval[i]
         }
     }
