839334d5d48fb7e116a42980e8bfd30e7f1ccfa7
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-24 23:32:08 -0600

    Time: 2 ms (79.58%) | Memory: 64 MB (80.38%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,42 +1,29 @@
 function maxArea(height: number[]): number {
 
     /*
-    dynamic programming where we store the volume
-    two pointers starting on opposing ends
-    increment the lowest of the two height[i]
-    do this while l < r, break when = then return the container volume and the 2 indexes
-    */
-
-    /*
-    if this required returning the indices, what would I do?
-    I would need to have a data structure that saved these indices
-    If only one unique answer:
-      -> variable for tracking the max volume
-      -> use this variable as a key for a map that contains the 2 index numbers
-
-    at the end we simply look up the profit and return the index numberse
-
+    use a 2 pointer approach where l starts at 0 and r is at end
+    increment the lower of the 2
+    save maxVolume as we go
+    return when pointers meet our max value
     */
 
     let l = 0;
     let r = height.length - 1;
-    let maxAmount = -Infinity;
+    let maxVolume = -Infinity;
 
-    while(l < r) {
+    while (l < r) {
 
-        //calculate max
-        maxAmount = Math.max(maxAmount, Math.min(height[r], height[l]) * (r - l));
+        //set max volume
+        maxVolume = Math.max(maxVolume, Math.min(height[l], height[r]) * (r - l));
 
-        //increment
-        if(height[l] < height[r]) {
+        //increment whichever is lower
+        if ( height[l] < height[r]) {
             l++;
-            console.log(l);
         } else {
             r--;
         }
-
     }
 
-    return maxAmount;
-
+    return maxVolume;
+    
 };
