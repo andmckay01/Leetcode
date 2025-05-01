ff5c72bd7eb205793d0fc8ad87eb7d711d52fd03
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-04-30 23:19:25 -0600

    Time: 49 ms (7.92%) | Memory: 64.8 MB (10.04%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -12,41 +12,43 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
-    let newList = new ListNode(0);
-    let node = newList;
+    /*
+    don't need to reverse
+    define the numbers or 0 so that they can be added up
+    define carry (dynamic variable)
+    */
+
     let carry = 0;
+    let newList = new ListNode(0); //starting at 0, will return .next
+    let node = newList;
 
-    while(l1 || l2 || carry > 0) {
+    while (l1 || l2 || carry > 0) {
 
-        //get values to add together
         let l1Value = l1 ? l1.val : 0;
         let l2Value = l2 ? l2.val : 0;
 
-        //add them together
-        let newValue = l1Value + l2Value + carry;
-        carry = 0; //reset carry right after we use it
+        let newValue = l1Value + l2Value + carry; //set newValue
+        carry = 0; //reset carry
 
-        //compute the newvalue and carry as needed
-        if(newValue > 9) {
-            newValue = newValue % 10;
-            carry = 1;
+        if (newValue > 9) {
+            newValue = newValue % 10; //set newValue
+            carry = 1 //set carry
+            console.log('new value: ', newValue);
         }
-        
-        //add to the next node in the newList and increment
-        node.next = new ListNode(newValue);
-        node = node.next;
 
-        //increment values
-        if(l1) {
+        //use new value for new node
+        node.next = new ListNode(newValue);
+        node = node.next; //increment
+
+        //increment linked lists
+        if (l1) {
             l1 = l1.next;
         }
 
-        if(l2) {
-            l2 = l2.next
+        if (l2) {
+            l2 = l2.next;
         }
-
     }
 
     return newList.next;
-    
 };
