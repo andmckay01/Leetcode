4fa4afb6af358f753220d2ec7acdb11de8efabc9
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-03 22:25:10 -0600

    Time: 2 ms (99.40%) | Memory: 57.1 MB (86.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,22 +1,19 @@
 function isAnagram(s: string, t: string): boolean {
 
-    /*
-    consist of lowercase english letters (no characters)
-    we can simply iterate through both of these O(n + m) time space -> reduces to O(n)
-    if the inputs contain unicode characters we would use
-    */
-
-    if(s.length !== t.length) return false
-
-    let characterArray = new Array(26).fill(0); //array with 26 characters
-
-    for(let i=0; i<s.length; i++) {
-
-        characterArray[s[i].charCodeAt(0) - 97]++;
-        characterArray[t[i].charCodeAt(0) - 97]--
-
+    //base case
+    if (s.length != t.length) {
+        return false;
     }
 
+    let characterArray = new Array(26).fill(0);
+
+    //loop through
+    for (let i = 0; i < s.length; i++) {
+        characterArray[s[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+        characterArray[t[i].charCodeAt(0) - "a".charCodeAt(0)]--;
+    }
+
+    //if every value is 0, return true else return false
     return characterArray.every(freq => !freq);
     
 };
