38df0fc6d587c47711f9c6ff3fb613b273bc81e1
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-09 09:44:49 -0600

    Time: 19 ms (26.22%) | Memory: 65 MB (10.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,39 +1,37 @@
 function topKFrequent(nums: number[], k: number): number[] {
 
     /*
-    steps:
-    1. iterate through the numbers and add the count to a map
-    2. add the numbers to the array with the index as the count
-    3. iterate in reverse, adding the numbers to a res, and then return
+    we need to iterate through once, adding items to a map
+    then we iterate through the map, adding items to an array in reverse order of the count
+    must be O(n) where we simply need to loop through twice
     */
 
-    let count = new Map(); //number: count
-    let freq = Array.from({length: nums.length + 1}, () => [])
+    //will add to a map with the number as the key and updating the count as the value
+    let freq = new Map();
+    let orderedArray = Array.from({length: nums.length + 1}, () => [])
 
-    //count the occurence of each number
-    for(let i=0; i<nums.length; i++) {
-        if(count.has(nums[i])) {
-            count.set(nums[i], count.get(nums[i]) + 1);
+    //initial loop to add counts
+    for (let num of nums) {
+        if (freq.has(num)) {
+            freq.set(num, freq.get(num) + 1); //add 1 to the count
         } else {
-            count.set(nums[i], 1);
+            freq.set(num, 1);
         }
     }
 
-    //add to the freq array with the index being our count
-    for(let [num, frequency] of count) {
-        freq[frequency].push(num)
-        // freq[frequency] = num;
+    //second loop to add counts to the array
+    for (let [num, count] of freq) {
+        orderedArray[count].push(num);
     }
 
-    //iterate in reverse
+    //iterate in reverse, adding numbers to res object
     let res = [];
-    for(let i=freq.length-1; i>=0; i--) {
-        for(let value of freq[i]) {
-            res.push(value)
-            if(res.length >= k) {
-                return res
+    for (let i = orderedArray.length - 1; i >= 0; i--) {
+        for (let value of orderedArray[i]) {
+            res.push(value);
+            if (res.length >= k) {
+                return res;
             }
         }
     }
-    
 };
