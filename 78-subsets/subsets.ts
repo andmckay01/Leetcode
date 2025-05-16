89f66f3f64f431a21b4c9c7727de6e3e0359a8a4
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-15 21:46:55 -0600

    Time: 472 ms (4.12%) | Memory: 66.5 MB (5.19%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -1,39 +1,29 @@
 function subsets(nums: number[]): number[][] {
 
-    /*
-    need to use recursive dfs for each number to build the subset
-    only add to the map if it does not exist
-    each time we recurse down, we add to a subset array (deciding to include or not include the next number)
-    backtracking
-
-    build the subset as we decide to add or not
-    we then run dfs 
-    */
-
     let res = [];
     let subset = [];
 
-    //i is defaulted to zero
     function dfs(i = 0) {
 
-        //if we are at the end, add the subset to our result
+        console.log('res: ', res);
+        console.log('subset: ', subset);
+
         if (i >= nums.length) {
             res.push([...subset]);
             return
         }
 
-        //resursive call on the other indexes
         subset.push(nums[i]);
         dfs(i + 1);
 
-        //other recursive call without the inclusion of the number
         subset.pop();
         dfs(i + 1);
 
     }
 
-    //run dfs and return
+    //call dfs and return res
     dfs();
     return res;
+    
 
 };
