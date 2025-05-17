c07560958923dc109311a70e22a8e70c082f9e79
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-16 22:24:18 -0600

    Time: 3 ms (45.79%) | Memory: 63.5 MB (84.31%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,30 +1,29 @@
 function maxArea(height: number[]): number {
 
     /*
-    use left and right pointer
-    move whichever is has the lower height
-    save the max volume as a dynamic variable
-    return when l and r =
+    two pointer solution starting at left and right
+    dynamic variable with the max volume
     */
 
     let l = 0;
     let r = height.length - 1;
-    let maxVolume = 0;
+    let maxVolume = -Infinity;
 
     while (l < r) {
 
-        let newVolume = Math.min(height[r], height[l]) * (r - l)
-        maxVolume = Math.max(maxVolume, newVolume);
+        let currentVolume = Math.min(height[r], height[l]) * (r - l);
+
+        maxVolume = Math.max(maxVolume, currentVolume);
 
         //increment whichever is lower
-        if (height[l] < height[r]) {
-            l++
+        if (height[l] > height[r]) {
+            r--;
         } else {
-            r--
+            l++;
         }
 
     }
 
-    return maxVolume
+    return maxVolume;
     
 };
