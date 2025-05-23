19a4fccc6c8fb7e18fccdabe2e250284f134ec79
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-22 23:29:05 -0600

    Time: 0 ms (100.00%) | Memory: 54.6 MB (99.86%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -3,20 +3,13 @@ function subsets(nums: number[]): number[][] {
     let res = [];
     let subset = [];
 
-    /*
-    add to subset as we recursively call dfs
-    add to res if we are at the end
-    */
-
     function dfs(i = 0) {
 
-        //if we are at the final number we add subset to res
         if (i >= nums.length) {
-            res.push([...subset])
+            res.push([...subset]);
             return
         }
 
-        //otherwise, we call with current num added and without current num added
         subset.push(nums[i]);
         dfs(i + 1);
 
@@ -25,7 +18,7 @@ function subsets(nums: number[]): number[][] {
 
     }
 
-    //call dfs and return res
+    //call dfs and return
     dfs();
     return res;
     
