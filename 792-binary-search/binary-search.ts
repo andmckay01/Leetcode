ceb3e5526d050f91ba75f02e6f228588e9f54f61
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-27 22:07:18 -0600

    Update binary-search.ts

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,30 +1,23 @@
 function search(nums: number[], target: number): number {
 
     /*
-    ascending tells us we can use binary search
-    use a pointer to decide where to search
-    check if number is more or less than what we access
-
-    O(n log n)
-
+    ascending order tells us we can use binary search
+    O(log n) is also binary search time complexity
     */
 
     let l = 0;
     let r = nums.length - 1;
 
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
 
-    while(l <= r) {
-
-        let x = Math.floor((r + l) / 2);
-
-        if(nums[x] < target) {
-            l = x + 1;
-        } else if (nums[x] > target) {
-            r = x - 1;
+        if (target < nums[m]) {
+            r = m - 1;
+        } else if (target > nums[m]) {
+            l = m + 1;
         } else {
-            return x
+            return m
         }
-
     }
 
     return -1
