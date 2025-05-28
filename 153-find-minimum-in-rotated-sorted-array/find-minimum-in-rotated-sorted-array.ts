79ad68e2933c2355f8f2ac05e4fdc47dff5f8640
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-05-28 13:36:59 -0600

    Time: 0 ms (100.00%) | Memory: 55.8 MB (25.57%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,18 +1,27 @@
 function findMin(nums: number[]): number {
 
+    /*
+    sorted so we can still use binary search
+    dynamic programming to find the min??
+    we need to find the pivot point. The digit to the right of the pivot point is the min
+    We can find the pivot point by checking if i + 1 is less than i. If it is, simply return nums[i + 1]
+    */
+
     let l = 0;
     let r = nums.length - 1;
 
-    while(l < r) {
-        let m = l + Math.floor((r-l) / 2); //midpoint without bit overflow
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2); //midpoint, upper
 
-        if(nums[r] > nums[m]) {
+        //this means all values between m + 1 and r aren't needed
+        if (nums[r] > nums[m]) {
             r = m;
         } else {
-            l = m + 1;
+            l = m + 1
         }
+
     }
 
-    return nums[l]
-
+    return nums[l];
+    
 };
