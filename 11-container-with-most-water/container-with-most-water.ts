14822721093426524fcbb90e5f715730bf257d2b
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-07 21:37:05 -0600

    Time: 3 ms (40.06%) | Memory: 63.6 MB (61.39%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,21 +1,13 @@
 function maxArea(height: number[]): number {
 
-    /*
-    two pointer solution starting at left and right
-    dynamic variable with the max volume
-    */
-
     let l = 0;
     let r = height.length - 1;
     let maxVolume = -Infinity;
 
     while (l < r) {
+        let newVolume = Math.min(height[l], height[r]) * (r - l);
+        maxVolume = Math.max(maxVolume, newVolume);
 
-        let currentVolume = Math.min(height[r], height[l]) * (r - l);
-
-        maxVolume = Math.max(maxVolume, currentVolume);
-
-        //increment whichever is lower
         if (height[l] > height[r]) {
             r--;
         } else {
