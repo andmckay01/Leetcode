4d72011505d08b954680530c3ee342e3e7d73885
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-09 23:52:09 -0600

    Time: 4 ms (56.93%) | Memory: 57.1 MB (41.93%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -3,13 +3,14 @@ function twoSum(nums: number[], target: number): number[] {
     let hashmap = new Map();
 
     for(let i = 0; i < nums.length; i++) {
-        let numberNeeded = target - nums[i]
+        let numberNeeded = target - nums[i];
 
-        if(hashmap.has(numberNeeded)) {
+        if (hashmap.has(numberNeeded)) {
             return [hashmap.get(numberNeeded), i]
         }
 
-        hashmap.set(nums[i], i)
+        hashmap.set(nums[i], i);
+
     }
     
 };
