4105bbd4ac1bde138e0ad6eb7955d84d5c8afd99
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-22 22:48:28 -0600

    Time: 0 ms (100.00%) | Memory: 59.2 MB (8.57%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -3,27 +3,20 @@ function search(nums: number[], target: number): number {
     let l = 0;
     let r = nums.length - 1;
 
-    //once they equal, we should have the number we're searching for
+    //lower bound
     while(l <= r) {
-        let m = Math.floor(l + (r - l)/2); //left bound
+        let m = l + Math.floor(( r - l ) / 2);
 
-        console.log('r: ', r);
-        console.log('l: ', l);
-        console.log('m: ', m);
-
-        //if midpoint is greater than target
+        //if less than target, increase l
         if (nums[m] == target) {
             return m;
-        //if midpoint is greater than target we shift r down to midpoint
-        } else if (nums[m] > target) {
-            r = m - 1; //left bound
-        //else we shift l up to midpoint + 1
-        } else {
+        } else if (nums[m] < target) {
             l = m + 1;
+        } else {
+            r = m - 1;
         }
-
     }
 
-    return -1;
+    return -1
     
 };
