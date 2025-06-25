5c1e53860453dfe1111876a354ad47337efba8e6
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-25 14:48:02 -0600

    Time: 46 ms (32.51%) | Memory: 74.6 MB (15.88%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -1,46 +1,54 @@
 function threeSum(nums: number[]): number[][] {
 
     /*
-    our numbers array must first be sorted
-    a, b, c
-    create a loop to find our a, then we use binary search to find our b and c that satisfies the condition
-    save anything that satisfies to our output array as itself an array
-    1 main loop driven by nums (leftmost number)
-      we need to have a condition where if the current index = previous index, continue to increment
-    2nd loop is two sum
+    sort everything
+
+    main check to make sure b != a
+
+    main loop to drive the loop with an a
+    sub loop using pointers b and c to find combinations that work
+      -> use binary search methodology to do this
+
+    if a combination works, add it to res
+
     */
 
     nums.sort((a, b) => a - b);
     let res = [];
 
-    for(let i=0; i<nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
 
-        if(nums[i] == nums[i-1]) { //i>0 is crucial so that we don't ever continue on the first one
-            continue //we don't want to use the same digit for a more than once
+        //condition to decide if we've already seen this a number
+        //do we need this to do an extra check??
+        if (nums[i] == nums[i - 1]) {
+            continue; //continue to the next number so we do not build a duplicate triplet
         }
 
-        let a = i //current pointer
-        let b = i+1; //one past
-        let c = nums.length - 1; //endpoint
+        //define all of the triplets. a will stay in place, b and c will use binary search pattern 
+        let a = i;
+        let b = i + 1;
+        let c = nums.length - 1;
 
-        while(b < c) {
+        //need a check to make sure b is not the same as the last one
 
+        while (b < c) {
+
+            //before anything, we need to make sure that b is distinct
             while(b < c && a == b) {
                 b++;
             }
-
+            
             let sum = nums[a] + nums[b] + nums[c];
 
-            //increment at the end based on condition
-            if(sum > 0) {
+            if (sum > 0) {
                 c--;
             } else if (sum < 0) {
                 b++;
             } else {
-                res.push([nums[a], nums[b], nums[c]])
-                b++;
+                res.push([nums[a], nums[b], nums[c]]);
                 c--;
-                while(nums[b] == nums[b-1] && b < c) {
+                b++;
+                while (nums[b] == nums[b-1] && b < c) {
                     b++;
                 }
             }
