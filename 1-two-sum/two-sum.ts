f450d8f7304becd118d68bb275aeb341c9a66a1e
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-27 22:12:04 -0700

    Update two-sum.ts

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -5,11 +5,10 @@ function twoSum(nums: number[], target: number): number[] {
     for(let i = 0; i < nums.length; i++) {
         let numberNeeded = target - nums[i];
 
-        if (hashmap.has(numberNeeded)) {
-            return [i, hashmap.get(numberNeeded)];
-        }
+        if (hashmap.has(numberNeeded)) return [hashmap.get(numberNeeded), i]
 
-        hashmap.set(nums[i], i)
+        hashmap.set(nums[i], i);
+
     }
     
 };
