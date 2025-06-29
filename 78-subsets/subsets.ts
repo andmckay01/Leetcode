831c85afac856383f9a1987f3f1cb2f15eaa1b08
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-06-28 22:42:09 -0700

    Time: 0 ms (100.00%) | Memory: 58.6 MB (24.90%) - LeetSync

--- 78-subsets/subsets.ts
+++ 78-subsets/subsets.ts
@@ -3,14 +3,14 @@ function subsets(nums: number[]): number[][] {
     let subset = [];
     let res = [];
 
-    function dfs(i = 0) {
+    function dfs( i = 0 ) {
         if (i >= nums.length) {
             res.push([...subset]);
             return;
         }
 
         subset.push(nums[i]);
-        dfs(i + 1);
+        dfs(i + 1)
 
         subset.pop();
         dfs(i + 1);
