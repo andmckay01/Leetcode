ad35d1b96cb99ca8810ef1d372de6edde89735cc
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-07-02 13:44:53 -0600

    Time: 10 ms (6.86%) | Memory: 68.2 MB (5.16%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -3,11 +3,15 @@ function maxProfit(prices: number[]): number {
     let minPrice = Infinity;
     let maxProfit = 0;
 
+    //find min price and max price and then calculate at the end and return
+    //if not profit, return 0
+
     for (let p of prices) {
+
         let profit = p - minPrice;
         minPrice = Math.min(minPrice, p);
         maxProfit = Math.max(maxProfit, profit);
-        
+
     }
 
     return maxProfit;
