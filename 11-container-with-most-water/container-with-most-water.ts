d3a8c23e381c203d00d0a1a4f83e7a46f4ce45b2
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-07-01 21:29:56 -0600

    Time: 2 ms (76.27%) | Memory: 63.7 MB (59.60%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -1,12 +1,18 @@
 function maxArea(height: number[]): number {
 
+    /*
+    use l and r pointers
+    calculate the water volume then shift whichever pointer corresponds to a smaller height
+    */
+
     let l = 0;
     let r = height.length - 1;
     let maxVolume = -Infinity;
 
     while (l < r) {
-        let newVolume = Math.min(height[l], height[r]) * (r - l);
-        maxVolume = Math.max(maxVolume, newVolume);
+
+        let volume = Math.min(height[l], height[r]) * (r - l);
+        maxVolume = Math.max(volume, maxVolume);
 
         if (height[l] > height[r]) {
             r--;
