58b3f999e33648ea90856a74820c1a3a07fc51d2
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-07-17 23:05:45 -0600

    Time: 2 ms (92.58%) | Memory: 61.8 MB (83.42%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -17,30 +17,29 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     let dummy = head;
 
     while (l1 || l2 || carry > 0) {
+        
         let l1Value = l1 ? l1.val : 0;
         let l2Value = l2 ? l2.val : 0;
-        let newValue = l1Value + l2Value + carry;
-        carry = 0;
+        let newNumber = l1Value + l2Value + carry;
+        carry = 0
 
-        if (newValue > 9) {
-            newValue = newValue % 10;
+        if (newNumber >= 10) {
             carry = 1;
+            newNumber = newNumber % 10;
         }
 
-        dummy.next = new ListNode(newValue);
+        dummy.next = new ListNode(newNumber);
         dummy = dummy.next;
 
         if (l1) {
             l1 = l1.next;
-        }
+        };
 
         if (l2) {
             l2 = l2.next;
-        }
-
+        };
     }
 
     return head.next;
-
     
 };
