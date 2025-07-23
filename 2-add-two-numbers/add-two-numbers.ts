852c8a4bd559d141730145524450135d9941faf9
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-07-22 22:55:32 -0600

    Update add-two-numbers.ts

--- 2-add-two-numbers/add-two-numbers.ts
+++ 2-add-two-numbers/add-two-numbers.ts
@@ -12,32 +12,33 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
-    let carry = 0;
     let head = new ListNode(0);
     let dummy = head;
+    let carry = 0;
 
-    while (l1 || l2 || carry > 0) {
-        
+    while(l1 || l2 || carry > 0) {
+
         let l1Value = l1 ? l1.val : 0;
         let l2Value = l2 ? l2.val : 0;
         let newNumber = l1Value + l2Value + carry;
-        carry = 0
+        carry = 0;
 
-        if (newNumber >= 10) {
-            carry = 1;
+        if (newNumber > 9) {
             newNumber = newNumber % 10;
+            carry = 1
         }
 
         dummy.next = new ListNode(newNumber);
         dummy = dummy.next;
 
-        if (l1) {
-            l1 = l1.next;
-        };
-
         if (l2) {
             l2 = l2.next;
-        };
+        }
+
+        if (l1) {
+            l1 = l1.next;
+        }
+        
     }
 
     return head.next;
