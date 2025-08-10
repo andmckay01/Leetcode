17c708d070dfd7f620ea5dd8c52fded1e1ffc154
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-08-09 20:14:53 -0600

    Update 3sum.ts

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -3,13 +3,12 @@ function threeSum(nums: number[]): number[][] {
     /*
     sort everything
 
-    main check to make sure b != a
+    main check to make sure b does not equal a
 
     main loop to drive the loop with an a
-    sub loop using pointers b and c to find combinations that work
-      -> use binary search methodology to do this
+    sub loop for b and c to find combinations that work
 
-    if a combination works, add it to res
+    add to res if it works
 
     */
 
@@ -18,18 +17,14 @@ function threeSum(nums: number[]): number[][] {
 
     for (let i = 0; i < nums.length; i++) {
 
-        //condition to decide if we've already seen this a number
-        //do we need this to do an extra check??
         if (nums[i] == nums[i - 1]) {
             continue; //continue to the next number so we do not build a duplicate triplet
         }
 
-        //define all of the triplets. a will stay in place, b and c will use binary search pattern 
+        //a will stay in place, b and c will use binary search
         let a = i;
         let b = i + 1;
         let c = nums.length - 1;
-
-        //need a check to make sure b is not the same as the last one
 
         while (b < c) {
 
