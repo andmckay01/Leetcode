a942338741a5f8f89d436768cea55520e573a959
Mckay Anderson <115378137+andmckay01@users.noreply.github.com>
2025-08-24 21:19:40 -0600

    Update maximum-depth-of-binary-tree.ts

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -14,7 +14,7 @@
 
 function maxDepth(root: TreeNode | null): number {
 
-    if (!root) return 0;
+    if (!root) return 0; 
 
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     
